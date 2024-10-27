e06a22dc76759a4e7a821585e238b104e6070f71
Lhk <212792772@qq.com>
2024-10-28 01:32:29 +0800

    Feat: 调整 题目提交接口的位置

--- src/main/java/com/lhk/kkoj/controller/QuestionController.java
+++ src/main/java/com/lhk/kkoj/controller/QuestionController.java
@@ -11,10 +11,15 @@ import com.lhk.kkoj.constant.UserConstant;
 import com.lhk.kkoj.exception.BusinessException;
 import com.lhk.kkoj.exception.ThrowUtils;
 import com.lhk.kkoj.model.dto.question.*;
+import com.lhk.kkoj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.lhk.kkoj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.lhk.kkoj.model.entity.Question;
+import com.lhk.kkoj.model.entity.QuestionSubmit;
 import com.lhk.kkoj.model.entity.User;
+import com.lhk.kkoj.model.vo.QuestionSubmitVO;
 import com.lhk.kkoj.model.vo.QuestionVO;
 import com.lhk.kkoj.service.QuestionService;
+import com.lhk.kkoj.service.QuestionSubmitService;
 import com.lhk.kkoj.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -38,6 +43,9 @@ public class QuestionController {
 
     @Resource
     private UserService userService;
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
 
     // region 增删改查
 
@@ -280,5 +288,63 @@ public class QuestionController {
         boolean result = questionService.updateById(question);
         return ResultUtils.success(result);
     }
+
+
+
+    /**
+     * 题目提交/ 取消题目提交
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return resultNum 本次题目提交变化数
+     */
+    @PostMapping("/submit")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能题目提交
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+
+    /**
+     * 分页获取题目提交信息列表（管理员查询题目提交信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @return Page<QuestionSubmit>
+     */
+    @PostMapping("/submit/list/page")
+    public BaseResponse<Page<QuestionSubmit>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        return ResultUtils.success(questionSubmitPage);
+    }
+
+    /**
+     * 分页获取脱敏的题目提交信息列表
+     *
+     * @param questionSubmitQueryRequest
+     * @param request Page<QuestionSubmitVO>
+     * @return
+     */
+    @PostMapping("/submit/list/page/vo")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitVOByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                           HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        //获取登录用户
+        final User loginUser = userService.getLoginUser(request);
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
+
 
 }
