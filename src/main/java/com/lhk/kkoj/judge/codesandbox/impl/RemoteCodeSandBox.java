4f2de127844091ba993dde90abd2e663e6a78a3d
Lhk <212792772@qq.com>
2024-10-28 01:31:22 +0800

    Feat: 新增 使用 HttpUtil 实现远程代码沙箱调用逻辑

--- src/main/java/com/lhk/kkoj/judge/codesandbox/impl/RemoteCodeSandBox.java
+++ src/main/java/com/lhk/kkoj/judge/codesandbox/impl/RemoteCodeSandBox.java
@@ -1,16 +1,35 @@
 package com.lhk.kkoj.judge.codesandbox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.lhk.kkoj.common.ErrorCode;
+import com.lhk.kkoj.exception.BusinessException;
 import com.lhk.kkoj.judge.codesandbox.CodeSandBox;
 import com.lhk.kkoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.lhk.kkoj.judge.codesandbox.model.ExecuteCodeResponse;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 远端 docker 代码沙箱
  */
 public class RemoteCodeSandBox implements CodeSandBox {
+
+    private static final String AUTH_REQUEST_HEADER = "apiAuth";
+    private static final String AUTH_REQUEST_SECRET = "secret";
+
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("远程代码沙箱");
-        return null;
+        String url = "http://192.168.136.131:8102/executeJavaCode";
+        String body = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
+                .body(body)
+                .execute()
+                .body();
+        if (StringUtils.isBlank(responseStr)){
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "远程代码沙箱运行失败");
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 }
