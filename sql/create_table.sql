a057f5fade3aca6e83b781a9c15d7624960c5e2c
Lhk <212792772@qq.com>
2024-11-07 16:08:57 +0800

    Feat: 调整 题目提交表的编码格式，修正无法提交中文的问题

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -60,7 +60,7 @@ create table if not exists question_submit
     isDelete   tinyint  default 0                 not null comment '是否删除',
     index idx_questionId (questionId),
     index idx_userId (userId)
-) comment '题目提交表';
+) comment '题目提交表' collate = utf8mb4_unicode_ci;;
 
 
 -- 帖子表
